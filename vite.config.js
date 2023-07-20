38c2c21f93a3cac7a269d69806f2096efbcddca4
iHeslop <iheslop182@gmail.com>
2023-07-20 23:43:29 +1000

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/iHeslop.github.io/";
-  }
-
-  return config;
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
 });
