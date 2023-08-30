aa43025873c73bde76dbfbe16ec07d0d822a36f8
iHeslop <iheslop@optusnet.com.au>
2023-08-30 12:24:01 +1000

    Update

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import { styles } from "../styles";
-import FadeIn from "react-fade-in/lib/FadeIn";
 
 const LoadingScreen = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center">
-      <FadeIn transitionDuration={800}>
         <div className="loading-screen bg-white flex justify-center items-center h-full w-full">
           <TypeAnimation
             sequence={["", 2000, "Isaac Heslop.", 2000, "Portfolio.", 2000]}
@@ -16,7 +14,6 @@ const LoadingScreen = () => {
             repeat={0}
           />
         </div>
-      </FadeIn>
     </div>
   );
 };
